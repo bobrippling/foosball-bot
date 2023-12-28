7aa9bdb6a7b5d9f3d7d7ababfad3bc1e559987d6
Lena <lena.wrblwsk@gmail.com>
2023-12-28 15:23:48 +0000

    timetable and items initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import { Items } from "./Items.jsx";
+import { Timetable } from "./Timetable.jsx";
 import { Bookings } from "./Bookings.jsx";
+import './App.css';
 
 export const App = () => {
   return (
     <div className="app">
-      <Bookings />
+      <h1>Social Hub Operator</h1>
+      <div class="container">
+        <div class="Items">
+          <Items />
+        </div>
+        <div class="Timeslots">
+          <Bookings />
+          <Timetable />
+        </div>
+      </div>
     </div>
   );
 };
