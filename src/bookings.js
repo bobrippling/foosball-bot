5155d287b2042643becf63ce5525a29b60d93957
Rob Pilling <robpilling@gmail.com>
2023-12-28 11:06:35 +0000

    Pretend to load bookings

--- src/bookings.js
+++ src/bookings.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 export const useBookings = () => {
+  const [bookings, setBookings] = React.useState();
+  const r = React.useRef();
+
+  if (bookings == null && !r.scheduled) {
+    setTimeout(
+      () => {
+        setBookings([
+          {
+            name: "Rob, Lena & Tawseef vs. javascript",
+          },
+        ]);
+      },
+      1000,
+    );
+    r.scheduled = true;
+  }
+
   return {
-    isLoading: false,
-    data: [
-      {
-        name: "Rob, Lena & Tawseef vs. JavaScript",
-      }
-    ]
+    isLoading: bookings == null,
+    data: bookings,
   };
 };
