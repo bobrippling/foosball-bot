6b9569a3319755cf2af4c8d0c6efcb3c1356daef
Rob Pilling <robpilling@gmail.com>
2023-12-28 16:38:37 +0000

    Reformat time.js

--- src/time.js
+++ src/time.js
@@ -1,28 +1,28 @@
 const pad0 = n => n < 10 ? "0" + n : n;
 
 export class Time {
-	constructor(h, m, duration = 30) {
-		this.h = h;
-		this.m = m;
-		this.duration = duration;
-	}
+  constructor(h, m, duration = 30) {
+    this.h = h;
+    this.m = m;
+    this.duration = duration;
+  }
 
-	static at(h, m) {
-		return new Time(h, m);
-	}
+  static at(h, m) {
+    return new Time(h, m);
+  }
 
-	overlaps(other) {
-		const thisStart = this.h * 60 + this.m;
-		const otherStart = other.h * 60 + other.m;
+  overlaps(other) {
+    const thisStart = this.h * 60 + this.m;
+    const otherStart = other.h * 60 + other.m;
 
-		const thisEnd = thisStart + this.duration - 1;
-		const otherEnd = otherStart + other.duration - 1;
+    const thisEnd = thisStart + this.duration - 1;
+    const otherEnd = otherStart + other.duration - 1;
 
-		return thisStart <= otherStart && otherStart < thisEnd
-			|| thisStart <= otherEnd && otherEnd < thisEnd;
-	}
+    return thisStart <= otherStart && otherStart < thisEnd
+      || thisStart <= otherEnd && otherEnd < thisEnd;
+  }
 
-	format() {
-		return `${pad0(this.h)}:${pad0(this.m)}`;
-	}
+  format() {
+    return `${pad0(this.h)}:${pad0(this.m)}`;
+  }
 }
